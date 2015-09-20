58024358140d290f20664229e78f78786f11eeff
Michael Lutaaya <michael@lutaaya.com>
2015-09-20 16:27:46 -0400

    Replace tabs with spaces; add some TODOs

--- ftpServer.c
+++ ftpServer.c
@@ -7,122 +7,111 @@
 #include <unistd.h>
 #include "ftpDefs.h"
 
+// TODO: Duplicate code shouldn't exist
 void errorMessage(char *msg) {
-
-	perror(msg);
-	exit(1);
-
+  perror(msg);
+  exit(1);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
+	// Do not continue unless all arguments have been provided
+  if (argc < 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
 
-	int	checkSocketConnectionSuccessful, portNo, newSocketForClient;
-	socklen_t clientLen;
-	char 	buffer[MAX_BUFF_LEN];
-	struct 	sockaddr_in server_addr, client_addr;
+  int  checkSocketConnectionSuccessful, portNo, newSocketForClient;
+  socklen_t clientLen;
+  char   buffer[MAX_BUFF_LEN];
+  struct   sockaddr_in server_addr, client_addr;
 
+  // Create a socket and check if it was successful or not
+  checkSocketConnectionSuccessful = createSocket();
 
-	/* Do a basic error check if the command line argument is not complete throw an error am using
-         * exit(1) to flag an error which is un successful termination */
-	if (argc < 2) {
+  if(checkSocketConnectionSuccessful < 0) {
+    errorMessage("Error opening socket");
+  }
 
-                fprintf(stderr,"ERROR, no port was specified in your command line argument\n");
-                exit(1);
+  /* Before I use my server address I will like to clear and
+   * release it of anything that was using it previously so I
+   * can use it from a fresh and free of all junks LOL */
+  bzero((char*) &server_addr, sizeof(server_addr));
 
-        }
+  // Now I want to store my port no which I will retrieve from my command line argument
+  portNo = atoi(argv[1]);
 
+  // Initialize my server address
+  server_addr.sin_family     = AF_INET;
+  server_addr.sin_addr.s_addr   = INADDR_ANY;
+  server_addr.sin_port     = htons(portNo);
 
-	// Create a socket and check if it was successful or not
-	checkSocketConnectionSuccessful = createSocket();
+  // Bind my socket
+  if(bind(checkSocketConnectionSuccessful, (struct sockaddr *) &server_addr,sizeof(server_addr)) < 0)  {
+    errorMessage("Error Binding the IP address, try again! ");
+  }
 
-	if(checkSocketConnectionSuccessful < 0) {
-		errorMessage("Error opening socket");
-	}
+  // Listen for connection, the number 10 is the maximum no. of clients the server should allow
+  listen(checkSocketConnectionSuccessful, 10);
 
-	/* Before I use my server address I will like to clear and
-	 * release it of anything that was using it previously so I
-	 * can use it from a fresh and free of all junks LOL */
-	bzero((char*) &server_addr, sizeof(server_addr));
+  clientLen = sizeof(client_addr);
 
+  // Accept connection when client sends a request
 
-	// Now I want to store my port no which I will retrieve from my command line argument
-	portNo = atoi(argv[1]);
+  do {
+    newSocketForClient = accept(checkSocketConnectionSuccessful, (struct sockaddr *) &client_addr, &clientLen);
 
-	// Initialize my server address
-	server_addr.sin_family 		= AF_INET;
-	server_addr.sin_addr.s_addr 	= INADDR_ANY;
-	server_addr.sin_port 		= htons(portNo);
+    if(newSocketForClient < 0) {
 
-	// Bind my socket
-	if(bind(checkSocketConnectionSuccessful, (struct sockaddr *) &server_addr,sizeof(server_addr)) < 0)	{
-		errorMessage("Error Binding the IP address, try again! ");
-	}
+      errorMessage("Error accepting connection\n");
+    }
 
-	// Listen for connection, the number 10 is the maximum no. of clients the server should allow
-	listen(checkSocketConnectionSuccessful, 10);
+  // Just to make sure my buffer is all clear and set to use I will clear it again just to be safe
+    bzero(buffer, 256);
 
-	clientLen = sizeof(client_addr);
+  // read message received from client
+    readMessageFromClient(newSocketForClient, &buffer);
 
+  // send message to client
+    writeMessageToClient(newSocketForClient, &buffer);
+  } while (1);
 
-	// Accept connection when client sends a request
-
-	do {
-		newSocketForClient = accept(checkSocketConnectionSuccessful, (struct sockaddr *) &client_addr, &clientLen);
-
-		if(newSocketForClient < 0) {
-
-			errorMessage("Error accepting connection\n");
-		}
-
-	// Just to make sure my buffer is all clear and set to use I will clear it again just to be safe
-		bzero(buffer, 256);
-
-	// read message received from client
-		readMessageFromClient(newSocketForClient, &buffer);
-
-	// send message to client
-		writeMessageToClient(newSocketForClient, &buffer);
-	} while (1);
-
-	return 0;
+  return 0;
 }
-
-
 
 int createSocket() {
-	return socket(AF_INET, SOCK_STREAM, 0);
+	// TODO: this could be moved to a networking.c file
+  return socket(AF_INET, SOCK_STREAM, 0);
 }
-
 
 int readMessageFromClient(int clientSock, char (*buff)[MAX_BUFF_LEN]) {
 
-	int n;
+  int n;
 
-	n = read(clientSock, buff, MAX_BUFF_LEN);
+  n = read(clientSock, buff, MAX_BUFF_LEN);
 
-	if(n < 0) {
-		errorMessage("Error reading from the client socket");
-		return -1;
-	}
+  if(n < 0) {
+    errorMessage("Error reading from the client socket");
+    return -1;
+  }
 
-	else {
-		printf("Message received From the client: %s\n", (char *)buff);
-		return 0;
-	}
+  else {
+    printf("Message received From the client: %s\n", (char *)buff);
+    return 0;
+  }
 
 }
 
 int writeMessageToClient(int clientSock, char (*buff)[MAX_BUFF_LEN]) {
 
-	int n;
+  int n;
 
-	char *message = "I got your message";
-	n = write(clientSock, message, sizeof(message));
+  char *message = "I got your message";
+  n = write(clientSock, message, sizeof(message));
 
-	if(n < 0) {
-		errorMessage("Error writing to the client socket");
-		return -1;
-	}
+  if(n < 0) {
+    errorMessage("Error writing to the client socket");
+    return -1;
+  }
 
-	return 0;
+  return 0;
 }
